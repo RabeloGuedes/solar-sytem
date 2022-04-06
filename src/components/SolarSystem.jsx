a76c785a1b95bfc8f624ff742763cbf7dddc63f1
RabeloGuedes <krepscredian@gmail.com>
2022-04-06 17:19:33 -0300

    feat: Component PlanetCard foi adicionado ao Component SolarSystem e a lista de planetas foi passada para o PlanetCard.

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,6 +9,14 @@ class SolarSystem extends React.Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        { planets.map((planet) => {
+          const singlePlanet = (<PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />);
+          return singlePlanet;
+        }) }
       </>
 
     );
